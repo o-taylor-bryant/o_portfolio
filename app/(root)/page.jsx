ed309d40de215ac85a5b38ee5fb2baf8eb18f182
taylorreeneb <196473818+o-taylor-bryant@users.noreply.github.com>
2025-10-02 11:16:10 -0400

    Updated my resume!

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -298,7 +298,7 @@ const MyPage = () => {
                     <div className="flex flex-row space-x-4">
                       <Button variation="primary">
                         <a
-                          href="https://tinyurl.com/otaylorbryantresume2025"
+                          href="https://tinyurl.com/taylorbryantresume"
                           target="_blank"
                           rel="noopener noreferrer"
                           className="flex items-center"
@@ -318,7 +318,7 @@ const MyPage = () => {
                     </div>
                     <div className="flex flex-col items-center">
                       <Image
-                        src="/image/otaylorbryantresume2025 QR.png"
+                        src="/image/taylorbryantresume-400.png"
                         alt="Taylor Bryant Resume QR Code"
                         width={112}
                         height={112}
